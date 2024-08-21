34dc246d6e62eeed20bb6c6cf6f6c11eb6301270
Shabana-Fakhar <shabanafakhar0861@gmail.com>
2024-08-21 11:15:27 +0500

    day 1 practise

---
+++ Main.js
@@ -0,0 +1,14 @@
+var arr=[
+    {name:"shabana",age:19,color:"red"},
+    {name:"rimsha",age:20,color:"green"},
+    {name:"areej",age:21,color:"blue"},
+    {name:"saman",age:22,color:"orange"},
+]
+
+    
+
+
+for(var index =0; index <=arr.length; index++){
+    console.log(arr[index])
+}
+
